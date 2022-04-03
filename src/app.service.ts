9dde7cb320fbffd07fd5e90f2354dd6154f9ba4b
Igor Srdoč <igor@srdoc.si>
2022-04-03 19:06:01 +0200

    Change demo hello response again

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World of nestjs!';
+    return 'Hello World of nestjs again!';
   }
 }
